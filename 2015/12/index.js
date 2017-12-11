1e753d1f8bfebd39c543c9f9062716b94cbe1c51
James Rishe <jlrishe@users.noreply.github.com>
2017-12-12 03:44:27 +0900

    More refactoring of 2015-12

--- 2015/12/index.js
+++ 2015/12/index.js
@@ -1,42 +1,45 @@
 const { compose, map, curry, values, always, sum, ifElse, contains, both, applySpec, cond, equals, identity, T, type, applyTo } = require('ramda');
 const { probe } = require('../shared');
 
-// [*] -> Number
-const totalArray = excludeRed => compose(sum, map(total(excludeRed)));
-
-// Boolean -> [*] -> Boolean
-const excludeValues = excludeRed => both(always(excludeRed), contains('red'));
-
-// Boolean -> Object -> Number
-const totalObject = excludeRed => compose(
-    ifElse(excludeValues(excludeRed), always(0), totalArray(excludeRed)),
-    values
-);
-
 // {a: b} -> b -> a -> b
 const lookup = curry((lu, def, val) => val in lu ? lu[val] : def);
 
-// Boolean -> String -> * -> Number
-const action = (excludeRed) => lookup({ 
-    Array : totalArray(excludeRed), 
-    Object: totalObject(excludeRed), 
-    Number: identity 
-}, always(0));
-
-// Boolean -> * -> Number
-const total = curry((excludeRed, val) => compose(
-    applyTo(val),
-    action(excludeRed),
-    type
-)(val));
+const totaller = excludeRed => {
+    // [*] -> Number
+    const totalArray = arr => sum(map(total, arr));
+    
+    // Boolean -> [*] -> Boolean
+    const excludeValues = both(always(excludeRed), contains('red'));
+    
+    // Boolean -> Object -> Number
+    const totalObject = compose(
+        ifElse(excludeValues, always(0), totalArray),
+        values
+    );
+    
+    // Boolean -> String -> * -> Number
+    const action = lookup({ 
+        Array : totalArray, 
+        Object: totalObject, 
+        Number: identity 
+    }, always(0));
+    
+    // * -> * -> Number
+    const actionForVal = compose(action, type);
+    
+    // * -> Number
+    const total = val => action(type(val))(val);   
+    
+    return total;
+};
 
 // String -> *
 const parse = str => JSON.parse(str);
 
 // String -> Number
-const p1 = compose(total(false), parse);
+const p1 = compose(totaller(false), parse);
 // String -> Number
-const p2 = compose(total(true) , parse);
+const p2 = compose(totaller(true) , parse);
 
 module.exports = {
     solution: {
