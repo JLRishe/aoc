68b3d0d998fde5fc19741177008f72c3b4b9ab75
James Rishe <jlrishe@users.noreply.github.com>
2017-12-18 03:07:21 +0900

    Simplify 2015-08

--- 2015/08/index.js
+++ 2015/08/index.js
@@ -1,29 +1,30 @@
 const { compose, map, sum } = require('ramda');
+const { applySpec, filter } = require('ramda');
 var _ = require('lodash');
 
-var literalToValue = 
-    literal => literal
-        .replace(/^"|"$/g, '')
-        .replace(/\\"|\\x[\da-f][\da-f]|\\\\/g, function (match) {
-            if (match === '\\\\') {
-                return '\\';
-            }
-            if (match === '\\"') {
-                return '"';
-            }
-            return String.fromCharCode(parseInt(match.substring(2), 16));
-        });
+// String -> String
+const literalToValue = l => JSON.parse(l.replace(/\\x([a-f0-9]{2})/g, '\\u00$1'));
         
-var sizeDifference = literal => literal.length - literalToValue(literal).length;
+// String -> Number
+const sizeDifference = literal => literal.length - literalToValue(literal).length;
 
-var valueToLiteral = 
-    value => '"' + value.replace(/["\\]/g, match => '\\' + match) + '"';
+// String -> String
+const valueToLiteral = v => JSON.stringify(v);
 
-var reverseSizeDifference = value => valueToLiteral(value).length - value.length;
+// String -> Number
+const reverseSizeDifference = value => valueToLiteral(value).length - value.length;
 
-const p1 = compose(sum, map(sizeDifference));
-const p2 = compose(sum, map(reverseSizeDifference));
+const totalSizeDifferences = f => xs => sum(map(f, xs));
+
+// [String] -> Number
+const p1 = totalSizeDifferences(sizeDifference);
+
+// [String] -> Number
+const p2 = totalSizeDifferences(reverseSizeDifference);
 
 module.exports = {
-    ps: [p1, p2]
+    solution: {
+        type: 'lines',
+        ps: [p1, p2]
+    }
 };
