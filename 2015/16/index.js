2efbc3508f2ab053e3188c9d598f8b0fd62ab48a
James Rishe <jlrishe@users.noreply.github.com>
2017-12-17 21:56:38 +0900

    Update 2015-16

--- 2015/16/index.js
+++ 2015/16/index.js
@@ -1,24 +1,29 @@
-var util = require('../shared/util');
-var _ = require('lodash');
+const { compose, map, curry, split, find, fromPairs } = require('ramda');
+const { applyPattern } = require('../shared');
 
-function parseSue(line) {
-    var parts1 = /^Sue (\d+): (.+)$/.exec(line),
-        parts2 = parts1[2]
-            .split(', ')
-            .map(part => /([a-z]+): (\d+)/.exec(part))
-            .map(part => [part[1], Number(part[2])]);
+// Sue is { num: String, items: { String: Number } }
 
-    return {
-        num: Number(parts1[1]),
-        items: _.zipObject(parts2)
-    };
-}
+// String -> { String: Number }
+const parseItems = compose(
+    map(Number),
+    fromPairs,
+    map(split(': ')),
+    split(', ')
+);
 
-var isMatch = 
-    (sue, items, itemMatcher) =>
-        Object.keys(sue.items).every(k => itemMatcher(sue.items[k], items[k], k));
+// String -> Sue
+const parseSue = compose(
+    ([, num, pets]) => ({ num, items: parseItems(pets) }),
+    applyPattern(/^Sue (\d+): (.+)$/),
+);
 
-function newMatcher(sueValue, foundValue, item) {
+// (Number -> Number -> String -> Boolean) -> { String: Number} -> Sue -> Boolean
+const isMatch = curry((itemMatcher, items, sue) =>
+    Object.keys(sue.items).every(k => itemMatcher(sue.items[k], items[k], k))
+);
+
+// (Number -> Number -> String -> Boolean)
+const newMatcher = (sueValue, foundValue, item) => {
     switch (item) {
         case 'cats':
         case 'trees': 
@@ -29,29 +34,38 @@ function newMatcher(sueValue, foundValue, item) {
         default:
             return sueValue === foundValue;
     }
-}
+};
 
-module.exports = (lines) => {
-    var sues = lines.map(parseSue);
-    
-    var itemsDetected = {
-        children: 3,
-        cats: 7,
-        samoyeds: 2,
-        pomeranians: 3,
-        akitas: 0,
-        vizslas: 0,
-        goldfish: 5,
-        trees: 3,
-        cars: 2,
-        perfumes: 1
-    };
 
-    // first answer    
-    var sue1 = sues.find(s => isMatch(s, itemsDetected, (sueValue, itemValue) => sueValue === itemValue));
-    
-    // second answer
-    var sue2 = sues.find(s => isMatch(s, itemsDetected, newMatcher));
-    
-    return [sue1, sue2];
+// (Number -> Number -> String -> Boolean)
+const simpleMatcher = (sueValue, itemValue) => sueValue === itemValue;
+
+const itemsDetected = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1
+};
+
+// (Number -> Number -> String -> Boolean) -> { String: Number } -> [Sue] -> Sue
+const findSue = matcher => find(isMatch(matcher, itemsDetected));
+
+// [Sue] -> Sue
+const p1 = findSue(simpleMatcher);
+
+// [Sue] -> Sue
+const p2 = findSue(newMatcher);
+
+module.exports = {
+    solution: {
+        ps: [p1, p2],
+        type: 'lines',
+        pre: parseSue
+    }
 };
