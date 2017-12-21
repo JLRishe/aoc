603bbb15cdda7c7826d903c99f50c6d4de403dfc
James Rishe <jlrishe@users.noreply.github.com>
2017-12-22 03:38:38 +0900

    Update day 19, part 1

--- 19/index.js
+++ 19/index.js
@@ -1,89 +1,48 @@
-var _ = require('lodash');
+const { compose, curry, map, split, tail, groupBy, prop, unnest, uniq, last, length } = require('ramda');
+const { probe, arrayMap } = require('aoc-helpers');
 
-function parseReplacement(line) {
-    var parts = line.split(' => ');
-    
-    return {
-        from: parts[0],
-        to: parts[1]
-    };
-}
+const splitCompound = compose(
+    tail,
+    split('|'),
+    s => s.replace(/[A-Z][a-z]?|e/g, m => '|' + m)
+);
 
-function flipReplacement(r) {
-    return {
-        from: r.to,
-        to: r.from
+const parseReplacement = compose(
+    ([from, to]) => ({ from, to: splitCompound(to) }),
+    split(' => '),
+);
+
+const parseReplacements = compose(
+    map(map(prop('to'))),
+    groupBy(prop('from')),
+    map(parseReplacement)
+);
+
+const parseInput = lines => ({ 
+    replacements: parseReplacements(lines.slice(0, lines.length - 2)),
+    compound: splitCompound(last(lines))
+});
+
+const applyReplacements = curry((replacements, el, i, arr) => map(
+    rep => [...arr.slice(0, i), ... rep, ...arr.slice(i + 1)],
+    replacements[el] || []
+));
+
+const applyAllReplacements = curry((replacements, compound) => compose(
+    uniq,
+    unnest,
+    arrayMap(applyReplacements(replacements))
+)(compound));
+
+const p1 = compose(
+    length,
+    ({ replacements, compound }) => applyAllReplacements(replacements, compound),
+    parseInput
+);
+
+module.exports = {
+    solution: {
+        ps: [p1],
+        type: 'lines'
     }
-}
-
-function performReplacement(replacement, line) {
-    var idx = line.indexOf(replacement.from);
-    var options = [];
-    
-    while (idx !== -1) {
-        var next = line.substring(0, idx) + replacement.to + line.substring(idx + replacement.from.length);
-        if (!_.contains(tried, next)) {
-            options.push(next);
-        }
-        idx = line.indexOf(replacement.from, idx + 1);
-    }
-    
-    return options;        
-}
-
-function doSingleReplacements(line, replacements) {
-    var allReplacements = replacements.map(r => performReplacement(r, line));
-    return Array.prototype.concat.apply([], allReplacements);
-}
-
-function doReplacements(lines, replacements) {
-    var lineReplacements = lines.map(l => doSingleReplacements(l, replacements));
-    var all = Array.prototype.concat.apply([], lineReplacements);
-    
-    return _.uniq(all);
-}
-
-var tried = [];
-var maxLength;
-var target;
-var minCount = 90000000;
-
-function tryInput(val, count, replacements) {
-    console.log(val);
-    if (val === target) { return count; }
-    var options = doSingleReplacements(val, replacements);
-    var oldTried = tried;
-    tried = _.uniq(oldTried.concat(options));
-
-    var results = options.filter(o => !_.contains(oldTried, o) && o.length <= maxLength)
-                         .map(o => tryInput(o, count + 1, replacements));
-                         
-    return results.filter(r => r > 0);
-}
-
-module.exports = (lines) => {
-    var replacements = lines.slice(0, lines.length - 1).map(parseReplacement);
-    
-    target = lines[lines.length - 1];
-    maxLength = target.length;
-    
-    var i = 0;
-        available = ['e'];
-    var allSoFar = [];
-    
-    var results = tryInput('e', 0, replacements);
-    
-    console.log(results);
-        
-/*    var flipped = replacements.map(flipReplacement);
-        
-    while(!_.contains(available, target)){
-        i += 1;
-        var nextStep = doReplacements(available, replacements);
-        available = nextStep.filter(m => !_.contains(allSoFar, m));
-        allSoFar = _.uniq(allSoFar.concat(available));
-        console.log(available);
-    }*/
-    
-    return [doReplacements([target], replacements).length];
 };
