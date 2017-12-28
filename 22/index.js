e5ca5f3fa8d565caf9ebbe2878bda2f48f037712
James Rishe <jlrishe@users.noreply.github.com>
2017-12-29 02:57:49 +0900

    Progress on day 22

--- 22/index.js
+++ 22/index.js
@@ -1,46 +1,84 @@
+const { compose, curry, map, filter, prop } = require('ramda');
 
-function adjustStat(character, stat, newValue, temp) {
-    var copy = Object.create(character),
-        tempCopy = temp ? Object.create(copy) : copy;
 
-    tempCopy.base = copy;
-    tempCopy[stat] = newValue;
-    
-    return tempCopy;
-}
+// State is { activeEffects, players: { player: Player, opponent: Opponent } }
 
-var spells = [
-    'Magic missile':  {
+const spells = {
+    'Magic missile': {
+        type: 'instant',
         cost: 53,
-        duration: 0,
-        turnEffect: players => players,
-        instantEffect: players => ({
-            player: players.player,
-            opponent:  adjustStat(players.opponent, 'hp', players.opponent.hp - 4)     
-        })
+        action: ({ opponent: { hp } }) => ({ opponent: { hp: hp - 4 } })
     },
     'Drain': {
+        type: 'instant',
         cost: 73,
-        duration: 0,
-        turnEffect: players => players,
-        instantEffect: players => ({
-            player: adjustStat(players.player, 'hp', players.player.hp + 2),
-            opponent: adjustStat(players.opponent, 'hp', players.opponent.hp - 2)
-        })
+        action: ({ player: { hp: playerHp }, opponent: { hp: opponentHp} }) => 
+            ({ player: { hp: playerHp + 2 }, opponent: { hp: opponentHp - 2 } });
     },
     'Shield': {
+        type: 'effect',
         cost: 113,
         duration: 6,
-        turnEffect: players => ({
-            player: adjustStat(players.player, 'armor', players.player.armor + 7, true),
-            opponent: opponent
-        }),
-        instantEffect: players => players
+        action: () => ({ player: { armor: 7 } })
     },
     'Poison': {
+        type: 'effect',
         cost: 173,
         duration: 6,
-        turnEffect: players => ({
-            player: player,
-            opponent: adjustStat(players.opponent, 'hp', players.opponent.hp - 3)
-];
+        action: ({ opponent: { hp } }) => ({ opponent: { hp: hp - 3 } })
+    },
+    'Recharge': {
+        type: 'effect',
+        cost: 229,
+        duration: 5,
+        action: ({ player: { mana }) => ({ player: { mana: mana + 101 })
+    }
+};
+
+const spellNames = keys(spells);
+
+const unavailableSpells = compose(
+    map(prop('name')),
+    filter(compose(gt(__, 1), prop('turns')))
+);
+
+const applySpell = (players, spellName) => {
+    const { player, opponent } = players;
+    const { player: playerChange, opponent: opponentChange } = spells[effectName].action(players);
+    const updatedPlayer = merge(player, playerChange || {});
+    const updatedOpponent = merge(opponent, opponentChange || {});
+    
+    return { player: updatedPlayer, opponent: updatedOpponent };
+}
+
+const applyEffects = ({ activeEffects, players }) => reduce(
+    applyEffect,
+    players,
+    activeEffects
+);
+
+const stepEffects = compose(
+    filter(({ turns }) => turns > 0),
+    map(e => merge(e, { turns: e.turns - 1 }))
+);
+
+const nextTurn = curry((state, spellName) => {
+    const players = applyEffects(state);
+    const steppedEffects = stepEffects(state.activeEffects);
+    
+    const spell = spells[spellName];
+    
+    const playersAfterSpell = spell.type === 'instant'
+        ? applySpell(players, spellName)
+        : players;
+        
+    
+});
+
+const pickSpells = (state) => {
+    const { activeEffects } = state;
+    const unavailable = unavailableSpells(activeEffects);
+    const available = without(unavailable, spellNames);
+    
+    return flatten(map(nextTurn(state), available)); 
+};
---
+++ test/22.js
@@ -0,0 +1,17 @@
+const day = '22';
+const dayPath = `../${day}`;
+
+const assert = require('assert');
+const { prepare } = require('aoc-runner');
+const dayContents = require(dayPath);
+const [p1, p2] = prepare(dayContents);
+
+describe(`day ${day}`, () => {
+    it('should work on samples for p1', () => {
+        throw new Error('not implemented');
+    });
+    
+    it('should work on samples for p2', () => {
+        throw new Error('not implemented');
+    });
+});
