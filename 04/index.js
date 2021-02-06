453fdbe924f2b051ef687272b27ca676d1fcf7ea
James Rishe <jlrishe@users.noreply.github.com>
2021-02-07 02:25:05 +0900

    04

--- 04/index.js
+++ 04/index.js
@@ -3,7 +3,7 @@ const { genInfinite, genFilter, genMap, genHead } = require('func-generators');
 const md5 = require('blueimp-md5');
 
 // String -> Generator { i: Number, hash: String }
-const hashes = key => genMap(i => ({ i, hash: md5(key + i) }), genInfinite);
+const hashes = key => genMap(i => ({ i, hash: md5(key + i) }), genInfinite());
 
 // String -> String -> Boolean
 const startsWith = (value, prefix) => value.indexOf(prefix) === 0;
